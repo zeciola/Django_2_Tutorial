ac8bc1be105a6c0af7c340723d2ddc4c7c3965b1
ZeCiola <jose.bricio@skyone.solutions>
2019-12-16 15:18:07 -0300

    update uri and views

--- meusite/polls/views.py
+++ meusite/polls/views.py
@@ -1,34 +1,54 @@
-from django.http import HttpResponse, HttpRequest, Http404
+from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
-
-
-def index(request: HttpRequest) -> HttpResponse:
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request=request, template_name='polls/index.html', context=context)
+from .models import Question, Choice
 
 
-def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five publish questions"""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    """Detail about question"""
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    """Result of poll"""
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponseRedirect or render:
+    """Vote in question method"""
     question = get_object_or_404(Question, pk=question_id)
 
-    return render(
-        request=request,
-        template_name='polls/detail.html',
-        context=dict(question=question)
-    )
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request=request,
+            template_name='polls/detail.html',
+            context=dict(
+                question=question,
+                error_message='Você não fez nem uma escolha!'
+            )
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
 
-
-def results(request: HttpRequest, question_id: int) -> HttpResponse:
-    response = f"Você está vendo os resultados da pesquisa de número: {question_id}"
-
-    return HttpResponse(response)
-
-
-def vote(request: HttpRequest, question_id: int) -> HttpResponse:
-    return HttpResponse(
-        f'Você votou na pesquisa de número: {question_id}'
-    )
+        return HttpResponseRedirect(
+            reverse(
+                'polls:results',
+                args=(question.id,)
+            )
+        )
