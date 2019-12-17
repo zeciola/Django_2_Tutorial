3155f681ea09aa1c459f877474d91a1fc5c18de8
ZeCiola <jose.bricio@skyone.solutions>
2019-12-17 14:09:23 -0300

    change admin pages layout

--- meusite/polls/admin.py
+++ meusite/polls/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-from .models import Question
+
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_publish_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    fieldsets = [
+        (None, dict(fields=['question_text'])),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
 
 admin.site.register(
-    Question
+    Question, QuestionAdmin
 )
